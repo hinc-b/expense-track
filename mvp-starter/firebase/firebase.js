624de6de9dde3b1aea57bb228bc6af597e2d33cb
Blazej Hinc <blazejh@google.com>
2023-03-17 13:46:38 +0100

    Authentication part

--- mvp-starter/firebase/firebase.js
+++ mvp-starter/firebase/firebase.js
@@ -17,6 +17,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDNiLRKDCT-fyq4l3fYnV9O7j4rLKVRMGs",
@@ -30,4 +31,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
