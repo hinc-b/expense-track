eba6dfc64655edfa67ff44d83c6d86114dd6d914
Blazej Hinc <blazejh@google.com>
2023-03-18 20:13:15 +0100

    references for services

--- mvp-starter/firebase/firebase.js
+++ mvp-starter/firebase/firebase.js
@@ -30,4 +30,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
