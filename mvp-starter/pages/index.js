bc5260acb33b3156c0ce966c59e5b434a9514ec9
Blazej Hinc <blazejh@google.com>
2023-03-19 18:05:35 +0100

    Configure FirebaseUI

--- mvp-starter/pages/index.js
+++ mvp-starter/pages/index.js
@@ -15,16 +15,27 @@
  * limitations under the License.
  */
 
-import { useEffect, useState } from 'react';
+
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
-import { Button, CircularProgress, Container, Dialog, Typography } from '@mui/material';
-import { auth } from '../firebase/firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import { Button, Container, Typography } from '@mui/material';
 import styles from '../styles/landing.module.scss';
+import { EmailAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+
+const REDIRECT_PAGE = '/dashboard';
+
+// Configure FirebaseUI
+const uiConfig = {
+  signInFlow: 'popup', // singIn flow with popup rather than redirect flow
+  signInSuccessUrl: REDIRECT_PAGE,
+  signInOptions: [
+    EmailAuthProvider.PROVIDER_ID,
+    GoogleAuthProvider.PROVIDER_ID,
+  ]
+}
 
 export default function Home() {
-  const router = useRouter();
+  const [login, setLogin] = useState(false);
 
   return (
     <div>
@@ -37,10 +48,14 @@ export default function Home() {
           <Typography variant="h1">Welcome to Expense Tracker!</Typography>
           <Typography variant="h2">Add, view, edit, and delete expenses</Typography>
           <div className={styles.buttons}>
-            <Button variant="contained" color="secondary">
+            <Button variant="contained" color="secondary"
+              onClick={() => setLogin(true)}>
               Login / Register
             </Button>
           </div>
+          <Dialog open={login} onClose={() => setLogin(false)}>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth}></StyledFirebaseAuth>
+          </Dialog>  
         </Container>
       </main>
     </div>);
