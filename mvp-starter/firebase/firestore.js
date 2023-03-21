e67ccc3168090a72214dc8ee70095f678b09ce5b
Blazej Hinc <blazejh@google.com>
2023-03-21 15:49:21 +0100

    setting up Firestore

--- mvp-starter/firebase/firestore.js
+++ mvp-starter/firebase/firestore.js
@@ -18,3 +18,10 @@
 import { addDoc, collection, deleteDoc, doc, getDocs, onSnapshot, orderBy, query, setDoc, where } from 'firebase/firestore'; 
 import { db } from './firebase';
 import { getDownloadURL } from './storage';
+
+const RECEIPTS_COLLECTION = 'receipts';
+
+export function addReceipt(uid, date, locationName, address, items, amount, imageBucket) {
+    addDoc(collection(db, RECEIPTS_COLLECTION), { uid, date, locationName, address, items, amount, imageBucket });
+}
+
